5393f48ee81578c7169d35b61e1d7befe64b3fe3
Guillaume Chau <alphadelta.fg@gmail.com>
2018-02-15 04:05:01 +0100

    Removed GitHub star link

--- app/src/components/AppFooter.vue
+++ app/src/components/AppFooter.vue
@@ -3,14 +3,7 @@
     <div>Made with <a href="https://vuejs.org/">Vue</a> + <a href="https://apollodata.com/">Apollo</a> + <router-link :to="{ name: 'about' }">❤️</router-link></div>
 
     <div>
-      <a
-        class="github-button"
-        href="https://github.com/Akryum/vue-summit-app"
-        data-icon="octicon-star"
-        data-size="large"
-        data-show-count="true"
-        aria-label="Star Akryum/vue-summit-app on GitHub"
-      >Star</a>
+      <a href="https://github.com/Akryum/vue-summit-app">Sources</a>
     </div>
   </footer>
 </template>
